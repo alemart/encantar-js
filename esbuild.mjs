e9a705cda8ef606b9e81bf357c92e7a022d4be2d
alemart <221477+alemart@users.noreply.github.com>
2025-01-18 22:41:08 -0300

    Move to esbuild

---
+++ esbuild.mjs
@@ -0,0 +1,77 @@
+import * as esbuild from 'esbuild';
+import { readFile } from 'fs/promises';
+
+const argv = process.argv.slice(2);
+const json = await readFile(new URL('package.json', import.meta.url));
+const pack = JSON.parse(json);
+const production = (argv.indexOf('--dev') < 0);
+const minify = (argv.indexOf('--minify') >= 0);
+const serve = (argv.indexOf('--serve') >= 0);
+
+const options = {
+    //entryPoints: ['src/main.ts'], // AR.default
+    stdin: {
+        contents: 'import AR from "./main.ts";\nmodule.exports = AR;',
+        resolveDir: 'src',
+        sourcefile: '__main.ts',
+    },
+    bundle: true,
+    minify: minify,
+    target: ['es2020'],
+    format: 'iife',
+    globalName: 'AR',
+    define: {
+        __AR_VERSION__: JSON.stringify(pack.version),
+        __AR_DEVELOPMENT_MODE__: String(!production),
+        __AR_WEBSITE__: JSON.stringify(pack.homepage),
+    },
+    legalComments: 'inline',
+    banner: { js: generateBanner() },
+    footer: { js: serve ? generateLiveReloadCode() : '' },
+    outfile: 'www/dist/' + (minify ? 'encantar.min.js' : 'encantar.js'),
+    sourcemap: !production && 'linked',
+    logLevel: 'info',
+};
+
+if(!serve) {
+    await esbuild.build(options);
+    process.exit(0);
+}
+
+const ctx = await esbuild.context(options);
+await ctx.watch();
+await ctx.serve({
+    host: '0.0.0.0',
+    port: 8000,
+    servedir: 'www',
+});
+
+function generateBanner()
+{
+    const { version, description, homepage, license } = pack;
+    const author = pack.author.replace('@', '(at)');
+    const year = new Date().getFullYear();
+    const date = new Date().toISOString();
+
+    return [
+    `/*!`,
+    ` * encantar.js version ${version}`,
+    ` * ${description}`,
+    ` * Copyright 2022-${year} ${author}`,
+    ` * ${homepage}`,
+    ` *`,
+    ` * @license ${license}`,
+    ` * Date: ${date}`,
+    `*/`
+    ].join('\n');
+}
+
+function generateLiveReloadCode()
+{
+    return `
+    (function liveReload() {
+       new EventSource('/esbuild').
+       addEventListener('change', () => location.reload());
+    })();
+    `;
+}
