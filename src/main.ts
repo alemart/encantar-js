54a2b82a09bd7e6ca8aefbfa39d7a192d37bcce0
alemart <221477+alemart@users.noreply.github.com>
2026-02-10 19:14:33 -0300

    Remove the explicit namespace

--- src/main.ts
+++ src/main.ts
@@ -26,117 +26,112 @@ import { Settings } from './core/settings';
 import { Session, SessionOptions } from './core/session';
 import { TrackerFactory } from './trackers/tracker-factory';
 import { SourceFactory } from './sources/source-factory';
-import { Viewport, ViewportSettings } from './core/viewport';
-import { Vector2 } from './geometry/vector2';
-import { Vector3 } from './geometry/vector3';
+import { Viewport as _Viewport, ViewportSettings } from './core/viewport';
+import { Vector2 as _Vector2 } from './geometry/vector2';
+import { Vector3 as _Vector3 } from './geometry/vector3';
 import { Utils } from './utils/utils';
 
 declare const __AR_WEBSITE__: string;
 
-/**
- * GPU-accelerated Augmented Reality for the web
- */
-export class AR
-{
-    /**
-     * Start a new session
-     * @param options
-     * @returns a promise that resolves to a new session
-     */
-    static startSession(options?: SessionOptions): SpeedyPromise<Session>
-    {
-        return Session.instantiate(options);
-    }
-
-    /**
-     * Checks if the engine can be run in the browser the client is using
-     * @returns true if the engine is compatible with the browser
-     */
-    static isSupported(): boolean
-    {
-        return Session.isSupported();
-    }
-
-    /**
-     * Engine version
-     */
-    static get version(): string
-    {
-        return Utils.engineVersion;
-    }
-
-    /**
-     * Speedy Vision
-     */
-    static get Speedy(): typeof Speedy
-    {
-        return Speedy;
-    }
-
-    /**
-     * Trackers
-     */
-    static get Tracker(): typeof TrackerFactory
-    {
-        return TrackerFactory;
-    }
-
-    /**
-     * Sources of data
-     */
-    static get Source(): typeof SourceFactory
-    {
-        return SourceFactory;
-    }
-
-    /**
-     * Create a viewport
-     * @param settings
-     * @returns a new viewport with the specified settings
-     */
-    static Viewport(settings: ViewportSettings): Viewport
-    {
-        return new Viewport(settings);
-    }
-
-    /**
-     * Create a new 2D vector
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @returns a new 2D vector with the provided coordinates
-     */
-    static Vector2(x: number, y: number): Vector2
-    {
-        return new Vector2(x, y);
-    }
-
-    /**
-     * Create a new 3D vector
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @param z z-coordinate
-     * @returns a new 3D vector with the provided coordinates
-     */
-    static Vector3(x: number, y: number, z: number): Vector3
-    {
-        return new Vector3(x, y, z);
-    }
-
-    /**
-     * Global Settings
-     */
-    static get Settings(): typeof Settings
-    {
-        return Settings;
-    }
-}
-
-// Freeze the namespace
-Object.freeze(AR);
-
 // Display a notice
 Utils.log(
-    `encantar.js version ${AR.version}. ` +
+    `encantar.js version ${Utils.engineVersion}. ` +
     `GPU-accelerated Augmented Reality for the web by Alexandre Martins. ` +
     __AR_WEBSITE__
 );
+
+/**
+ * Start a new session
+ * @param options
+ * @returns a promise that resolves to a new session
+ */
+export function startSession(options?: SessionOptions): SpeedyPromise<Session>
+{
+    return Session.instantiate(options);
+}
+
+/**
+ * Checks if the engine can be run in the browser the client is using
+ * @returns true if the engine is compatible with the browser
+ */
+export function isSupported(): boolean
+{
+    return Session.isSupported();
+}
+
+/**
+ * Engine version
+ */
+export const version = Utils.engineVersion;
+
+/**
+ * Global Settings
+ */
+export { Settings };
+
+/**
+ * Speedy Vision
+ */
+export { Speedy };
+
+/**
+ * Trackers
+ */
+export const Tracker = TrackerFactory;
+
+/**
+ * Sources of data
+ */
+export const Source = SourceFactory;
+
+/**
+ * Create a viewport
+ * @param settings
+ * @returns a new viewport with the specified settings
+ */
+export function Viewport(settings: ViewportSettings): _Viewport
+{
+    return new _Viewport(settings);
+}
+
+/**
+ * Create a new 2D vector
+ * @param x x-coordinate
+ * @param y y-coordinate
+ * @returns a new 2D vector with the provided coordinates
+ */
+export function Vector2(x: number, y: number): _Vector2
+{
+    return new _Vector2(x, y);
+}
+
+/**
+ * Create a new 3D vector
+ * @param x x-coordinate
+ * @param y y-coordinate
+ * @param z z-coordinate
+ * @returns a new 3D vector with the provided coordinates
+ */
+export function Vector3(x: number, y: number, z: number): _Vector3
+{
+    return new _Vector3(x, y, z);
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ * This utility lets you create an object of
+ * class T writing t = T(x) or t = new T(x)
+ * @returns a type/factory hybrid
+ * @internal
+ */
+/*
+function MakeFactory<T>(clazz: new (...args: any[]) => T)
+{
+    return new Proxy(clazz, {
+        apply(target, self, args) { return Reflect.construct(target, args); }
+    }) as typeof clazz & ((...args: any[]) => T);
+}
+export const Vector2 = MakeFactory(_Vector2) as typeof _Vector2 & ((x: number, y: number) => _Vector2);
+export const Vector3 = MakeFactory(_Vector3) as typeof _Vector3 & ((x: number, y: number, z: number) => _Vector3);
+*/
