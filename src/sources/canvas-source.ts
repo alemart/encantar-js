458a2673c179436090937bca0b21c0eecaaf6c79
alemart <221477+alemart@users.noreply.github.com>
2026-05-19 15:32:49 -0300

    Relicense the core library
    Version bump. Update description.

--- src/sources/canvas-source.ts
+++ src/sources/canvas-source.ts
@@ -1,20 +1,20 @@
 /*
  * encantar.js
- * GPU-accelerated Augmented Reality for the web
+ * GPU-accelerated Augmented Reality library for the web
  * Copyright (C) 2022-2026 Alexandre Martins <alemartf(at)gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <https://www.gnu.org/licenses/>.
  *
  * canvas-source.ts
  * HTMLCanvasElement-based source of data
